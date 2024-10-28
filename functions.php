ee2229abd84c427a525f8fba294d012c9021bdeb
antor9732 <antormahmud789@gmail.com>
2024-10-28 23:54:12 +0600

    bootstrap, custom.css file & bootstrap.js and main.js file load in our website

--- functions.php
+++ functions.php
@@ -12,12 +12,18 @@
 function codeSolution_css_js_file_calling(){
    wp_enqueue_style( 'codeSolution-style' , get_stylesheet_uri() );
    wp_register_style('bootstrap', get_template_directory_uri() .'/css/bootstrap.css', array(), 'v5.3.3','all');
+   wp_register_style('custom', get_template_directory_uri() .'/css/custom.cse', array(), 'v1.0.0','all');
+   wp_enqueue_style( 'bootstrap' );
+   wp_enqueue_style( 'custom' );
    
+   
+   //jQuery
+   wp_enqueue_script('jquery');
+   wp_enqueue_script( 'bootstrap', get_template_directory_uri() .'/js/bootstrap.js', array(), 'v5.3.3', true);
+   wp_enqueue_script( 'main', get_template_directory_uri() .'/js/main.js', array(), 'v1.0.0', true);
 
 }
 add_action( 'wp_enqueue_scripts', 'codeSolution_css_js_file_calling' );
-
-
 
 
 
