8f2af223814f003bca586d7c109a0e88463c6020
antor9732 <antormahmud789@gmail.com>
2024-10-30 11:06:11 +0600

    solve my custom css file issue. now all css are work

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@
 function codeSolution_css_js_file_calling(){
    wp_enqueue_style( 'codeSolution-style' , get_stylesheet_uri() );
    wp_register_style('bootstrap', get_template_directory_uri() .'/css/bootstrap.css', array(), 'v5.3.3','all');
-   wp_register_style('custom', get_template_directory_uri() .'/css/custom.cse', array(), 'v1.0.0','all');
+   wp_register_style('custom', get_template_directory_uri() .'/css/custom.css', array(), 'v1.0.0','all');
    wp_enqueue_style( 'bootstrap' );
    wp_enqueue_style( 'custom' );
    
@@ -28,7 +28,7 @@ add_action( 'wp_enqueue_scripts', 'codeSolution_css_js_file_calling' );
 
 //Google font Enqueue
 function codeSolution_add_google_font(){
-   wp_enqueue_style( 'codesolution_google-font', 'https://fonts.googleapis.com/css2?family=Kaisei+Decol&family=Oswald:wght@200..700&display=swap', false );
+   wp_enqueue_style( 'codesolution_google-font', 'https://fonts.googleapis.com/css2?family=Kaisei+Decol&family=Oswald:wght@200..700&display=swap', false);
 }
 add_action( 'wp_enqueue_scripts', 'codeSolution_add_google_font' );
 
