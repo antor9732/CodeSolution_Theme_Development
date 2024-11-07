271a5930805a8d1173dcdd9db626dfd3354dac79
antor9732 <antormahmud789@gmail.com>
2024-11-08 05:47:04 +0600

    Waker Menu Properties, Menu Description Style Css & function added

--- functions.php
+++ functions.php
@@ -85,4 +85,15 @@ add_action( 'wp_enqueue_scripts', 'codeSolution_add_google_font' );
 // Menu Register
 register_nav_menu('main_menu', __('Primary Menu', 'MahmudulHasan'));
 
+//Waker Menu Properties
+function  codesolution_description($item_output, $item, $depth, $args){
+if(!empty($item->description)){
+   $item_output = str_replace($args->link_after . '</a>', '<p class="menu-item-description">' . $item->description . '</p>' . $args->link_after . '</a>', $item_output);
+}
+return $item_output;
+
+}
+add_filter('walker_nav_menu_start_el', 'codesolution_description', 10,4);
+
+
 ?>
