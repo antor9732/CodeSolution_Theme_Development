390c8227d81fbc4860957be099d932761a04a11e
Md. Mahmudul Hasan <antormahmud789@gmail.com>
2024-12-05 12:48:06 +0600

    owl-carousel plugin added.

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -4,9 +4,13 @@ function codeSolution_css_js_file_calling(){
    wp_enqueue_style('codeSolution-style', get_stylesheet_uri());
    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array(), 'v5.3.3', 'all');
    wp_register_style('bxslider', get_template_directory_uri() . '/css/bxslider.min.css', array(), 'v4.2.17', 'all');
+   wp_register_style('owl.carousel.min', get_template_directory_uri() . '/css/owl.carousel.min.css', array(), 'v2.3.4', 'all');
+   wp_register_style('owl.theme.default.min', get_template_directory_uri() . '/css/owl.theme.default.min.css', array(), 'v2.3.4', 'all');
    wp_register_style('custom', get_template_directory_uri() . '/css/custom.css', array(), 'v1.0.0', 'all');
    wp_enqueue_style('bootstrap');
    wp_enqueue_style('bxslider');
+   wp_enqueue_style('owl.carousel.min');
+   wp_enqueue_style('owl.theme.default.min');
    wp_enqueue_style('custom');
 
 
@@ -14,6 +18,7 @@ function codeSolution_css_js_file_calling(){
    wp_enqueue_script('jquery');
    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.js', array(), 'v5.3.3', true);
    wp_enqueue_script('bxslider', get_template_directory_uri() . '/js/bxslider.min.js', array(), 'v4.2.17', true);
+   wp_enqueue_script('owl.carousel.min', get_template_directory_uri() . '/js/owl.carousel.min.js', array(), 'v2.3.4', true);
    wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', array(), 'v1.0.0', true);
 
 }
--- js/main.js
+++ js/main.js
@@ -3,4 +3,26 @@ jQuery(document).ready(function() {
         mode: 'fade',
       captions: true,
     });
+    jQuery("#owl-slider").owlCarousel({
+    loop:true,
+    nav:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },            
+        601:{
+            items:2
+        },            
+        960:{
+            items:3
+        },
+        1200:{
+            items:5
+        }
+    }
+    });
 });
